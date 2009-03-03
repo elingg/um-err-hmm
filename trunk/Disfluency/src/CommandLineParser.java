e49774ba72544e322614461ac08b8928996ab700
madhavan.anand <madhavan.anand@a9b0e5ec-f4d6-11dd-984d-ab748d24aa7e>
2009-03-03 09:20:24 +0000

    code done until able to extract word, prewords, postwords, pos tags, start and end times.

---
+++ trunk/Disfluency/src/CommandLineParser.java
@@ -0,0 +1,70 @@
+
+
+public class CommandLineParser {
+	// from http://journals.ecs.soton.ac.uk/java/tutorial/java/cmdLineArgs/parsing.html
+	public boolean m_verbose;
+	public String m_srcDir;
+	public Integer m_npregram, m_npostgram;
+	public void parseArguments(String[] args) {
+		int i = 0;
+		String arg;
+		m_verbose = false;
+		m_srcDir = "";
+
+		while (i < args.length && args[i].startsWith("-")) {
+			arg = args[i++];
+			// use this type of check for "wordy" arguments
+			if (arg.equals("-verbose")) {
+				System.out.println("verbose mode on");
+				m_verbose = true;
+			}
+
+			// use this type of check for arguments that require arguments
+			else if (arg.equals("-dir")) {
+				if (i < args.length)
+					m_srcDir = args[i++];
+				else
+					System.err.println("-dir requires a dirname");
+				if (m_verbose)
+					System.out.println("dir  = " + m_srcDir);
+			}
+			else if (arg.equals("-npregram")) {
+				if (i < args.length)
+					m_npregram = Integer.getInteger(args[i++]);
+				else
+					System.err.println("-npregram requires a number for npregram");
+				if (m_verbose)
+					System.out.println("npregram  = " + m_npregram);
+			}
+			else if (arg.equals("-npostgram")) {
+				if (i < args.length)
+					m_npostgram = Integer.getInteger(args[i++]);
+				else
+					System.err.println("-npostgram requires a number for npostgram");
+				if (m_verbose)
+					System.out.println("npostgram  = " + m_npostgram);
+			}
+			// use this type of check for a series of flag arguments
+//			else {
+//				for (j = 1; j < arg.length(); j++) {
+//					flag = arg.charAt(j);
+//					switch (flag) {
+//					case 'x':
+//						if (vflag) System.out.println("Option x");
+//						break;
+//					case 'n':
+//						if (vflag) System.out.println("Option n");
+//						break;
+//					default:
+//						System.err.println("ParseCmdLine: illegal option " + flag);
+//					break;
+//					}
+//				}
+//			}
+		}
+		if (i == args.length)
+			System.err.println("Usage: ParseCmdLine [-verbose] [-xn] [-output afile] filename");
+		else
+			System.out.println("Success!");
+	}
+}
