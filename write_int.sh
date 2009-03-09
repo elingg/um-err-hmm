23ca7e3f336c6e38c051522b2aef320ef6dd6798
madhavan.anand <madhavan.anand@a9b0e5ec-f4d6-11dd-984d-ab748d24aa7e>
2009-03-09 18:21:19 +0000

    should be able to generate interval files now for dev1 and dev2

---
+++ write_int.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This is how we run our code for extracting Disfluency features from xml and wav files:
+java -cp Disfluency/bin/ WordStream -dir ../corpus/LDC2009T01/CTSTreebankWithStructuralMetadata/data 
